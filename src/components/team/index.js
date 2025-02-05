3697071680cdc96e63d4c484667d793183cd577b
Pedro <pedroesnarriaga@gmail.com>
2025-02-05 16:22:07 -0400

    to be continue

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -1,23 +1,24 @@
 import Colaborator from '../Colaborator';
 import './Team.css';
 
-const Team = (props) => {
+const Team = (props, toDelete) => {
     const css = { backgroundColor: props.corSecundaria };
 
     return (
         props.devs.length > 0 ? (<section className='team' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.name}</h3>
             <div className='line'>
-            {props.devs.map((dev, index) => (
-                <Colaborator
-                    backgroundColor={props.corPrimaria}
-                    key={index}
-                    name={dev.name}
-                    role={dev.role}
-                    age={dev.age}
-                    interest={dev.interest}
-                />
-            ))}
+            {props.devs.map((dev, index) => {
+                return <Colaborator
+                backgroundColor={props.corPrimaria}
+                key={index}
+                name={dev.name}
+                role={dev.role}
+                age={dev.age}
+                interest={dev.interest}
+                delete={toDelete}
+            />
+            })}
             </div>
         </section>
         ) : null
