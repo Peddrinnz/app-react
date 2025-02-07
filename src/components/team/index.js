43d73efe03e5a56d195ae706245f37aeab9ce4cb
Pedro <pedroesnarriaga@gmail.com>
2025-02-07 15:49:29 -0400

    button color

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -1,27 +1,32 @@
 import Colaborator from '../Colaborator';
 import './Team.css';
 
-const Team = (props, toDelete) => {
-    const css = { backgroundColor: props.corSecundaria };
+const Team = ({ name, corPrimaria, corSecundaria, devs, toDelete }) => {
+    if (devs.length === 0) return null;
 
     return (
-        props.devs.length > 0 ? (<section className='team' style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.name}</h3>
-            <div className='line'>
-            {props.devs.map((dev, index) => {
-                return <Colaborator
-                backgroundColor={props.corPrimaria}
-                key={index}
-                name={dev.name}
-                role={dev.role}
-                age={dev.age}
-                interest={dev.interest}
-                delete={toDelete}
+        <section className='team' style={{ backgroundColor: corSecundaria }}>
+            <input
+                value={corSecundaria}
+                type='color'
+                className='input-color'
+                readOnly
             />
-            })}
+            <h3 style={{ borderColor: corPrimaria }}>{name}</h3>
+            <div className='line'>
+                {devs.map((dev, index) => (
+                    <Colaborator
+                        key={index}
+                        backgroundColor={corPrimaria}
+                        name={dev.name}
+                        role={dev.role}
+                        age={dev.age}
+                        interest={dev.interest}
+                        delete={toDelete}
+                    />
+                ))}
             </div>
         </section>
-        ) : null
     );
 };
 
